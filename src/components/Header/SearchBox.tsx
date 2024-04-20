7c7fbd48c7e2e0c5958f3469c9c8451c686bfdd4
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-04-20 14:07:17 +0900

    style: 검색박스 스타일 지정

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -6,13 +6,40 @@ const SearchBox = () => {
   const [query, setQuery] = useState('');
   return (
     <SearchBoxStyle>
-      <form>
-        <span>{/* <PiMagnifyingGlass /> */}</span>
-        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
-      </form>
+      <Icon>
+        <PiMagnifyingGlass />
+      </Icon>
+      <Input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="검색어를 입력하세요" />
     </SearchBoxStyle>
   );
 };
-const SearchBoxStyle = styled.div``;
+
+const SearchBoxStyle = styled.form`
+  display: flex;
+  align-items: center;
+  width: 30vw;
+  height: 36px;
+  padding: 0 1em;
+  border: 1px solid ${({ theme }) => theme.systemColor.border};
+  border-radius: 30px;
+`;
+
+const Icon = styled.span`
+  width: 20px;
+  height: 20px;
+  svg {
+    fill: ${({ theme }) => theme.systemColor.secondaryFont};
+  }
+`;
+
+const Input = styled.input`
+  background-color: transparent;
+  font-size: ${({ theme }) => theme.fontSize.small};
+  color: ${({ theme }) => theme.systemColor.secondaryFont};
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default SearchBox;
